6dac22a5bd35061825cf193080c94069f9c32657
SarveshD7 <sarvesh.ssd7@gmail.com>
2026-02-10 22:35:40 +0530

    Added Trade class, Trades vector alias

--- orderbook.cpp
+++ orderbook.cpp
@@ -104,7 +104,38 @@ class OrderModify{
 
 };
 
+ struct TradeInfo
+ {
+    OrderId orderId_;
+    Price price_;
+    Quantity quantity_;
 
+ };
+
+ class Trade
+ {
+    public:
+        Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade)
+        : bidTrade_{bidTrade},
+        askTrade_ {askTrade}
+        {}
+
+        const TradeInfo& GetBidTrade() const {bidTrade_;}
+        const TradeInfo& GetAskTrade() const {askTrade_;}
+
+    private:
+        TradeInfo bidTrade_;
+        TradeInfo askTrade_;
+
+ };
+
+using Trades = std::vector<Trade>;
+
+class Orderbook
+{
+    private:
+
+};
 
 int main(){
 
